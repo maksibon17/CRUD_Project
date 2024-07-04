f882cbc2c9eb2781b5926a8c42c14eca5cf3dfd6
Alenik <ponyo.working@outlook.com>
2024-07-05 00:03:38 +0300

    добавила @Mapping, исправила вывод в /authors

--- src/main/java/com/CRUD_Project/mappers/AuthorMapper.java
+++ src/main/java/com/CRUD_Project/mappers/AuthorMapper.java
@@ -3,6 +3,7 @@ package com.CRUD_Project.mappers;
 import com.CRUD_Project.dto.AuthorDTO;
 import com.CRUD_Project.entities.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -10,7 +11,14 @@ import java.util.List;
 @Mapper
 public interface AuthorMapper {
     AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "surname", source = "surname")
     AuthorDTO toDTO(Author author);
+
     List<AuthorDTO> toDTOList(List<Author> authors);
+
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "surname", source = "surname")
     Author toEntity(AuthorDTO authorDTO);
 }
