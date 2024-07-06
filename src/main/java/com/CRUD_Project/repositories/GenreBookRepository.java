a56711455b6a1f8797f4af6f1224d6a10473648a
maksibon17 <154748912+maksibon17@users.noreply.github.com>
2024-07-06 21:21:25 +0300

    Update GenreBookRepository.java

--- src/main/java/com/CRUD_Project/repositories/GenreBookRepository.java
+++ src/main/java/com/CRUD_Project/repositories/GenreBookRepository.java
@@ -1,7 +1,9 @@
 package com.CRUD_Project.repositories;
 
 import com.CRUD_Project.entities.GenreBook;
+import com.CRUD_Project.entities.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreBookRepository extends JpaRepository<GenreBook,Integer> {
+    GenreBook findByName(String name);
 }
