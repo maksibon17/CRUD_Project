f473e08f13187046049a3ccf934c721067cdf1c7
maksibon17 <154748912+maksibon17@users.noreply.github.com>
2024-07-06 21:26:37 +0300

    Add files via upload

--- src/main/java/com/CRUD_Project/mappers/ReaderMapper.java
+++ src/main/java/com/CRUD_Project/mappers/ReaderMapper.java
@@ -3,6 +3,7 @@ package com.CRUD_Project.mappers;
 import com.CRUD_Project.dto.ReaderDTO;
 import com.CRUD_Project.entities.Reader;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -10,7 +11,11 @@ import java.util.List;
 @Mapper
 public interface ReaderMapper {
     ReaderMapper INSTANCE = Mappers.getMapper(ReaderMapper.class);
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "email", source = "email")
     ReaderDTO toDTO(Reader reader);
     List<ReaderDTO> toDTOList(List<Reader> readers);
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "email", source = "email")
     Reader toEntity(ReaderDTO readerDTO);
 }
