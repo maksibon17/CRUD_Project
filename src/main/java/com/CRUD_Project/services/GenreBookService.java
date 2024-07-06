4e0b88d0f0a8832def6ace9b5bca8597016edd88
maksibon17 <154748912+maksibon17@users.noreply.github.com>
2024-07-06 21:24:27 +0300

    Add files via upload

--- src/main/java/com/CRUD_Project/services/GenreBookService.java
+++ src/main/java/com/CRUD_Project/services/GenreBookService.java
@@ -1,8 +1,14 @@
 package com.CRUD_Project.services;
 
+import com.CRUD_Project.dto.GenreBookDTO;
+import com.CRUD_Project.dto.ReaderDTO;
 import com.CRUD_Project.entities.GenreBook;
+import com.CRUD_Project.entities.Reader;
+import com.CRUD_Project.mappers.GenreBookMapper;
+import com.CRUD_Project.mappers.ReaderMapper;
 import com.CRUD_Project.repositories.GenreBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,37 +22,50 @@ public class GenreBookService {
     public GenreBookService() {
     }
 
-    public String findGenre(Integer id) {
-        Optional<GenreBook> genre = genreBookRepository.findById(id);
-        return genre.map(value -> "Найден жанр: " + value.getName())
-                .orElse("Жанра с id " + id + " не существует!");
+    public ResponseEntity<GenreBookDTO> findGenre(Integer id) {
+        Optional<GenreBook> genreBook = genreBookRepository.findById(id);
+        return genreBook.isPresent()
+                ? ResponseEntity.ok(GenreBookMapper.INSTANCE.toDTO(genreBook.get()))
+                : ResponseEntity.notFound().build();
     }
 
-    public List<GenreBook> findAll() {
-        return genreBookRepository.findAll();
+    public ResponseEntity<List<GenreBookDTO>> findAll() {
+        List<GenreBook> genreBooks = genreBookRepository.findAll();
+        List<GenreBookDTO> genreBookDTOListDTOList = GenreBookMapper.INSTANCE.toDTOList(genreBooks);
+        return ResponseEntity.ok(genreBookDTOListDTOList);
     }
 
-    public String create(String name) {
-        GenreBook genre = new GenreBook();
-        genre.setName(name);
-        genreBookRepository.save(genre);
-        return "Жанр с названием " + name + " создан успешно!";
+    public ResponseEntity<?> create(GenreBookDTO genreBookDTO) {
+        if (genreBookRepository.findByName(genreBookDTO.name())!=null) {
+            return ResponseEntity.badRequest().body("Жанр с названием " + genreBookDTO.name() + " уже существует!");
+        } else {
+            GenreBook genreBook = GenreBookMapper.INSTANCE.toEntity(genreBookDTO);
+            GenreBook savedGenre = genreBookRepository.save(genreBook);
+            GenreBookDTO savedGenreDTO = GenreBookMapper.INSTANCE.toDTO(savedGenre);
+            return ResponseEntity.ok(savedGenreDTO);
+        }
     }
 
-    public String delete(Integer id) {
-        Optional<GenreBook> genreOptional = genreBookRepository.findById(id);
-        if (genreOptional.isPresent()) {
-            genreBookRepository.delete(genreOptional.get());
-            return "Жанр с id " + id + " был удалён";
-        } else return "Жанра с id " + id + " не существует!";
+    public ResponseEntity<String> delete(Integer id) {
+        Optional<GenreBook> existingGenreBook = genreBookRepository.findById(id);
+        if (existingGenreBook.isPresent()) {
+            genreBookRepository.deleteById(id);
+            return ResponseEntity.ok("Жанр с id " + id + " успешно удалён.");
+        } else {
+            return ResponseEntity.status(404).body("Жанр с id " + id + " не существует!");
+        }
     }
 
-    public String edit(Integer id, String name) {
-        Optional<GenreBook> genreOptional = genreBookRepository.findById(id);
-        if (genreOptional.isPresent()) {
-            genreOptional.get().setName(name);
-            genreBookRepository.save(genreOptional.get());
-            return "Жанр с id " + id + " был изменён. Теперь его название " + name;
-        } else return "Жанра с id " + id + " не существует!";
+    public ResponseEntity<GenreBookDTO> edit(Integer id, GenreBookDTO genreBookDTO) {
+        Optional<GenreBook> existingGenreBook = genreBookRepository.findById(id);
+        if (existingGenreBook.isPresent()) {
+            GenreBook genreBook = existingGenreBook.get();
+            genreBook.setName(genreBookDTO.name());
+            GenreBook updatedGenreBook = genreBookRepository.save(genreBook);
+            GenreBookDTO updatedGenreBookDTO = GenreBookMapper.INSTANCE.toDTO(updatedGenreBook);
+            return ResponseEntity.ok(updatedGenreBookDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
