dadc42eb74446a4e5fa62b603783aa398b4c180b
Alenik <ponyo.working@outlook.com>
2024-07-08 06:03:23 +0300

    поправила @Mapping в BookMapper

--- src/main/java/com/CRUD_Project/mappers/BookMapper.java
+++ src/main/java/com/CRUD_Project/mappers/BookMapper.java
@@ -7,7 +7,6 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
-
 @Mapper
 public interface BookMapper {
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
@@ -18,7 +17,7 @@ public interface BookMapper {
 
     List<BookDTO> toDTOList(List<Book> books);
 
-    @Mapping(target = "author", ignore = true)
-    @Mapping(target = "genreBook", ignore = true)
+    @Mapping(target = "author.id", source = "authorId")
+    @Mapping(target = "genreBook.id", source = "genreBookId")
     Book toEntity(BookDTO bookDTO);
 }
